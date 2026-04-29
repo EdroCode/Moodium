c186e75ccadb0d3ae9e6574e9e0c1ffca7434ef1
PedroCoutinho <edr0c0de@protonmail.com>
2026-04-29 12:39:04 +0100

    feat: add sign out button

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useUser, useClerk } from "@clerk/nextjs";
+
+import { LogOut } from "lucide-react";
+
+export default function SignOutButton() {
+  const { signOut } = useClerk();
+
+  return (
+    <button
+      onClick={() => signOut({ redirectUrl: "/" })}
+      className="flex items-center gap-2 font-dm-sans text-lg text-primary  transition-all duration-200 p-2 rounded-lg cursor-pointer hover:font-semibold "
+    >
+      <LogOut className="w-4 h-4" strokeWidth={1.5} />
+      Sign out
+    </button>
+  );
+}
